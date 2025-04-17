b91af2f3f1817580676beca6d22826c9896ff235
aaradhyakul <aaradhyakul@gmail.com>
2025-04-17 07:30:50 +0000

    treesitter with error

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.g.mapleader = " " 
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -26,11 +27,27 @@ local plugins = {
   {
     'nvim-telescope/telescope.nvim', tag = '0.1.8',
     dependencies = { 'nvim-lua/plenary.nvim' }
-  }
+  },
+  {"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
 }
 local opts = {}
 
+-- treesitter
+local config = require("nvim-treesitter.configs")
+config.setup({
+  ensure_installed = {"lua", "javascript", "html", "c", "cpp", "css", "java", "make", "python", "rust", "sql", "svelte", "typescript"},
+  highlight = {enable = true},
+  indent = {enable = true},
+})
 
+-- lazy
 require("lazy").setup(plugins, opts)
+
+-- telescope
+local builtin = require("telescope.builtin")
+vim.keymap.set('n', '<C-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+-- colorscheme
 require("catppuccin").setup()
 vim.cmd.colorscheme "catppuccin"
