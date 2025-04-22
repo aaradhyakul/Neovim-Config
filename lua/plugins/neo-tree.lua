baaca9510ca0e07d31cc36c6080c473051b74f65
aaradhyakul <aaradhyakul@gmail.com>
2025-04-22 15:27:15 +0000

    line numbers, terminal key bindings

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -10,8 +10,19 @@ return {
   opts = {
   },
   config = function()
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        }
+      }
+    })
     vim.keymap.set('n', '<C-n>', ':Neotree filesystem toggle right<CR>', {})
-    vim.keymap.set('n', '<C-h>', '<C-w>h')
-    vim.keymap.set('n', '<C-l>', '<C-w>l')
+    vim.keymap.set({'n', 't'}, '<C-h>', '<C-w>h')
+    vim.keymap.set({'n', 't'}, '<C-l>', '<C-w>l')
+    vim.keymap.set({'n', 't'}, '<C-k>', '<C-w>k')
+    vim.keymap.set({'n', 't'}, '<C-j>', '<C-w>j')
   end
 }
